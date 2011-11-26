06bb60343be73dea5aad953ab4ff702629b9c788
zweizeichen <info@zweizeichen.org>
2011-11-26 14:53:12 +0100

    Updated version info

--- FreeOrion/Xcode/Version.cpp
+++ FreeOrion/Xcode/Version.cpp
@@ -1,7 +1,7 @@
 #include "../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.17 [GitMirror / SVN r4428] Xcode";
+    static const std::string retval = "post-v0.3.17 [GitMirror / SVN r4503] Xcode";
 }
 
 const std::string& FreeOrionVersionString()
